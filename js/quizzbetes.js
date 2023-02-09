ecae892baa341bf533a0f7ff9277ac7e924e38d4
Juliette Lega <legajuliette@gmail.com>
2023-02-09 14:14:09 +0100

    modif header (nav) et footer

--- js/quizzbetes.js
+++ js/quizzbetes.js
@@ -151,3 +151,32 @@ bouton.addEventListener("click", () => {
     }
   })
 
+  let quizz = document.querySelector('.derouleQuizz');
+  let info = document.querySelector('.derouleInfos');
+  
+  let sousQuizz = document.querySelector('.sousQuizz');
+  let sousInfos = document.querySelector('.sousInfos');
+  
+  let flecheQuizz = document.querySelector('.flecheQuizz');
+  let flecheInfos = document.querySelector('.flecheInfos');
+  
+  
+  quizz.addEventListener("click", () => {
+    if(getComputedStyle(sousQuizz).display != "block") {
+      sousQuizz.style.display = "block";
+      flecheQuizz.style.rotate = "90deg";
+    } else {
+      sousQuizz.style.display = "none";
+      flecheQuizz.style.rotate = "0deg";
+    }
+  })
+  
+  info.addEventListener("click", () => {
+    if(getComputedStyle(sousInfos).display != "block") {
+      sousInfos.style.display = "block";
+      flecheInfos.style.rotate = "90deg";
+    } else {
+      sousInfos.style.display = "none";
+      flecheInfos.style.rotate = "0deg";
+    }
+  })
